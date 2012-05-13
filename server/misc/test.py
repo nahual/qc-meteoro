b6fc9823024d7fa1ca248aecc20f26e97a409119
Mariano Simone <marianosimone@gmail.com>
2012-05-13 00:30:38 -0300

    borrando misc

--- server/misc/test.py
+++
@@ -1,8 +0,0 @@
-from yaml import load
-import unicodedata
-with open('cities.yaml', 'r') as f:
-    cities = load(f)
-    cities_list = cities.values()
-
-listita = map(lambda c: len(unicodedata.normalize('NFKD', c.decode("UTF-8")).encode('ascii', 'ignore')), cities_list)  
-print max(listita)
