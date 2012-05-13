28eb64d2029d7d21a750cea26b899ae3875f156d
Mariano Simone <marianosimone@gmail.com>
2012-05-13 19:48:58 -0300

    Agregando soporte en Yahoo! y devolviendo valor hardcodeado

--- server/yahoo.py
+++ server/yahoo.py
@@ -1,13 +1,28 @@
 from weather_provider import WeatherProvider
+import urllib2
+import json
+from datetime import datetime, timedelta
 
 class YahooWeatherProvider(WeatherProvider):
     def __init__(self, log_handlers):
         WeatherProvider.__init__(self, "MockWeahterProvider", log_handlers)
+        self.url = 'http://weather.yahooapis.com/forecastjson?w=%s&d=3&u=c'
 
     def get_cities(self, filtered):
         return [{"code":"466863", "name": "Mar del Plata"}]
 
     def get_forecast(self, city):
+        try:
+            data = urllib2.urlopen(self.url % city)
+            response = json.loads(data.read())
+            if 'Message' in response:
+                raise Exception(response['Message'])
+            return self._build_meteoro_result(response)
+        except Exception as e:
+            self._log.error("Error while getting weather from Yahoo", exc_info = True)
+            return {'error':'Problem with Yahoo!: %s' % e}
+
+    def _build_meteoro_result(self, response):
         today = datetime.now()
         return [
                 {'date': today.strftime("%Y-%m-%d"), 'temperature': 10, 'humidity': 60, 'chill': 10, 'status':'clear', 'min': 20, 'max': 10},
