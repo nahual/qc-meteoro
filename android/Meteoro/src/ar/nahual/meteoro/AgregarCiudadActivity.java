103635616b45cfb9707039605ea9e5b62d321388
marianosimone <marianosimone@gmail.com>
2012-05-26 15:26:03 -0300

    CLEANUP borrando import no usado

--- android/Meteoro/src/ar/nahual/meteoro/AgregarCiudadActivity.java
+++ android/Meteoro/src/ar/nahual/meteoro/AgregarCiudadActivity.java
@@ -14,7 +14,6 @@ import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.CheckBox;
 import ar.com.iron.android.extensions.activities.CustomActivity;
 import ar.com.iron.android.extensions.services.local.LocalServiceConnectionListener;
 import ar.com.iron.android.extensions.services.local.LocalServiceConnector;
