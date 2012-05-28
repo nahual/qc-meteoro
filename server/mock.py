4184bcf8c816a66c8725b5d8d30bc33577033f60
Mariano Simone <marianosimone@gmail.com>
2012-05-28 00:14:14 -0300

    Adaptando el mock a la nueva spec

--- server/mock.py
+++ server/mock.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from weather_provider import WeatherProvider
 from datetime import datetime, timedelta
 
@@ -16,10 +17,10 @@ class MockWeatherProvider(WeatherProvider):
     def get_forecast(self, city):
         today = datetime.now()
         return [
-                {'date': today.strftime(self.date_format), 'temperature': 10, 'humidity': 60, 'chill': 10, 'status':'clear', 'wind': 'NE 100KM/h', 'min': '', 'max': ''},
-                {'date': (today+timedelta(days=0)).strftime(self.date_format), 'temperature': '', 'humidity': '', 'chill': '', 'wind': '', 'status':'rainy', 'min': 20, 'max': 10},
-                {'date': (today+timedelta(days=1)).strftime(self.date_format), 'temperature': '', 'humidity': '', 'chill': '', 'wind': '', 'status':'rainy', 'min': 20, 'max': 10},
-                {'date': (today+timedelta(days=2)).strftime(self.date_format), 'temperature': '', 'humidity': '', 'chill': '', 'wind': '', 'status':'clear', 'min': 20, 'max': 10},
-                {'date': (today+timedelta(days=3)).strftime(self.date_format), 'temperature': '', 'humidity': '', 'chill': '', 'wind': '', 'status':'clear', 'min': 20, 'max': 10}
+                {'date': today.strftime(self.date_format), 'temperature': '10°C', 'humidity': '60%', 'chill': '10°C', 'status':'clear', 'min': '', 'max': ''},
+                {'date': (today+timedelta(days=0)).strftime(self.date_format), 'temperature': '', 'humidity': '', 'chill': '', 'status':'showers', 'min': '20°C', 'max': 10°C'},
+                {'date': (today+timedelta(days=1)).strftime(self.date_format), 'temperature': '', 'humidity': '', 'chill': '', 'status':'cloudy', 'min': '20°C', 'max': '10°C'},
+                {'date': (today+timedelta(days=2)).strftime(self.date_format), 'temperature': '', 'humidity': '', 'chill': '', 'status':'snow', 'min': '20°C', 'max': '10°C'},
+                {'date': (today+timedelta(days=3)).strftime(self.date_format), 'temperature': '', 'humidity': '', 'chill': '', 'status':'storm', 'min': '20°C', 'max': '10°C'}
                ]
 
